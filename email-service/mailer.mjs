ce26bc4261cec19de6078ecd34473138c32581d8
Satyam Pandey <93024366+Satyam039@users.noreply.github.com>
2025-12-23 10:55:02 +0530

    Update mailer.mjs

--- email-service/mailer.mjs
+++ email-service/mailer.mjs
@@ -7,18 +7,18 @@ const transporter = nodemailer.createTransport({
   port: 587,                   
   secure: false,
   auth: {
-    user: "satyampandey2219@gmail.com",  
-    pass: "ikpjelxnadfdkmxz",      
+    user: "test@gmail.com",  
+    pass: "xyz",      
   },
 });
 
 export async function sendAlertEmail({ website, analysis }) {
   const info = await transporter.sendMail({
-    from: `"Website Alert System" <satyampandey2219@gmail.com>`,
-    to: "satyam.pandey@cloudzent.com",
+    from: `"Website Alert System" <test@gmail.com>`,
+    to: "xyz@cloudzent.com",
     subject: `🚨 ALERT: Website Risk – ${website}`,
     text: analysis,
   });
 
-  console.log("✅ Alert email sent:", info.messageId);
+  console.log(" Alert email sent:", info.messageId);
 }
